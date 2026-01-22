92ef346f9cf93abdaf8cd591833d88726909b907
nocturnoClienteEj1 <jtrugui0302@iesmarquesdecomares.org>
2026-01-22 22:44:53 +0100

    version 22/01/2026

---
+++ API_Libros/src/test/java/ocm/pmdm/api_libros/ApiLibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package ocm.pmdm.api_libros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLibrosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ SpringbootProyecto/src/test/java/ocm/pmdm/springbootproyecto/SpringbootProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package ocm.pmdm.springbootproyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
