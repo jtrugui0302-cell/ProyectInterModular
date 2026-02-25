cee1d441360988cd7bc0b053b9fe911e692d13aa
jtrugui0302-cell <jtrugui0302@iesmarquesdecomares.org>
2026-02-25 19:51:48 +0100

    .

--- turismoapi/src/main/java/ocm/pmdm/turismoapi/repository/ComentarioRepository.java
+++
@@ -1,9 +0,0 @@
-package ocm.pmdm.turismoapi.repository;
-
-import ocm.pmdm.turismoapi.model.Comentario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ComentarioRepository extends JpaRepository<Comentario, Long>{
-}
